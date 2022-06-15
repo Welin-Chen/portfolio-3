dd050291bb0a9ed3d6d596fc16b04a10ae4995db
welin <a0929015591@gmail.com>
2022-06-15 23:28:52 +0800

    Projects add react-flip-move

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,45 +1,50 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
-function ProjectCard({ src, title, description, tags, demo, code }) {
-  return (
-    <div className="p-5 m-5 overflow-hidden transition duration-300 ease-in-out shadow-md opacity-100 hover:shadow-purple-500 shadow-purple-500 hover:shadow-lg rounded-xl hover:scale-105 hover:opacity-100">
-      <img
-        src={src}
-        alt={title}
-        className="object-fill transition duration-100 ease-in-out"
-      />
+const ProjectCard = forwardRef(
+  ({ src, title, description, tags, demo, code }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className="p-5 m-5 overflow-hidden transition duration-300 ease-in-out shadow-md opacity-100 hover:shadow-purple-500 shadow-purple-500 hover:shadow-lg rounded-xl hover:scale-105 hover:opacity-100"
+      >
+        <img
+          src={src}
+          alt={title}
+          className="object-fill transition duration-100 ease-in-out"
+        />
 
-      {/* info */}
-      <div className="p-5 mt-2 ">
-        <h2 className="text-2xl font-bold text-center text-purple-500 ">
-          {title}
-        </h2>
+        {/* info */}
+        <div className="p-5 mt-2 ">
+          <h2 className="text-2xl font-bold text-center text-purple-500 ">
+            {title}
+          </h2>
 
-        <div className="flex flex-wrap justify-between my-3">
-          {tags.map((tag, index) => (
-            <p key={index} className="text-lg font-semibold text-pink-500">
-              {tag}
-            </p>
-          ))}
+          <div className="flex flex-wrap justify-between my-3">
+            {tags.map((tag, index) => (
+              <p key={index} className="text-lg font-semibold text-pink-500">
+                {tag}
+              </p>
+            ))}
+          </div>
+        </div>
+
+        {/* button */}
+        <div className="flex justify-evenly">
+          <a href={demo} target="_blank" rel="noreferrer">
+            <button className="group button--gradient--outline">
+              <span className="button__span--gradient--outline">Visit</span>
+            </button>
+          </a>
+
+          <a href={code} target="_blank" rel="noreferrer">
+            <button className="group button--gradient--outline">
+              <span className="button__span--gradient--outline">Code</span>
+            </button>
+          </a>
         </div>
       </div>
-
-      {/* button */}
-      <div className="flex justify-evenly">
-        <a href={demo} target="_blank" rel="noreferrer">
-          <button className="group button--gradient--outline">
-            <span className="button__span--gradient--outline">Visit</span>
-          </button>
-        </a>
-
-        <a href={code} target="_blank" rel="noreferrer">
-          <button className="group button--gradient--outline">
-            <span className="button__span--gradient--outline">Code</span>
-          </button>
-        </a>
-      </div>
-    </div>
-  );
-}
+    );
+  }
+);
 
 export default ProjectCard;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,28 +1,31 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
 import { ProjectData } from "./ProjectData";
+import FlipMove from "react-flip-move";
 
 function Projects() {
   return (
-    <div className="div--container">
-      <p className="mt-5 w-fit p--title--purple">Projects</p>
+    <FlipMove>
+      <div className="div--container">
+        <p className="mt-5 w-fit p--title--purple">Projects</p>
 
-      <div className="sm:grid md:grid-cols-2 lg:grid-cols-3">
-        {ProjectData.map(
-          ({ id, src, title, description, tags, demo, code }, index) => (
-            <ProjectCard
-              key={id}
-              src={src}
-              title={title}
-              description={description}
-              tags={tags}
-              demo={demo}
-              code={code}
-            />
-          )
-        )}
+        <div className="sm:grid md:grid-cols-2 lg:grid-cols-3">
+          {ProjectData.map(
+            ({ id, src, title, description, tags, demo, code }, index) => (
+              <ProjectCard
+                key={id}
+                src={src}
+                title={title}
+                description={description}
+                tags={tags}
+                demo={demo}
+                code={code}
+              />
+            )
+          )}
+        </div>
       </div>
-    </div>
+    </FlipMove>
   );
 }
 
