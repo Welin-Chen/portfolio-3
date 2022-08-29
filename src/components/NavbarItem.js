a7b38a6016abcdc6e0205700cb884b8739c19071
welin <a0929015591@gmail.com>
2022-08-30 00:58:55 +0800

    改變整體字體顏色

--- src/components/NavbarItem.js
+++ src/components/NavbarItem.js
@@ -5,7 +5,7 @@ function NavbarItem({ path, title, Icon }) {
   return (
     <Link
       to={path}
-      className="flex flex-col items-center px-5 py-2 cursor-pointer transition--300-ease-in-out group lg:flex-row hover:text-gray-100 sm:py-0 hover:scale-110"
+      className="flex flex-col items-center px-5 py-2 cursor-pointer transition--300-ease-in-out group lg:flex-row hover:text-white sm:py-0 hover:scale-110"
     >
       <Icon className="h-6 mb-1" />
       <p className="ml-1 text-lg font-bold tracking-widest">{title}</p>
